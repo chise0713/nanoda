5dc1e1a5eff707df9804d1dfffa92795066e2418
チセ <123655015+chise0713@users.noreply.github.com>
2025-11-02 15:42:31 +0800

    Fix: proper `memory_size` handling &
    Chore: `MaybeUninit<T>` must be written before `T.assume_init()` &
            reduce heap allocation at string handling &
            re-format `nanoda version:`, output `cargo` before `git`

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,15 @@ const ITER: usize = 10_000_000;
 const EXIT_TOO_LARGE: u8 = 2;
 
 const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
-const GIT_DESCRIBE: &str = env!("VERGEN_GIT_DESCRIBE");
 const TARGET_TRIPLE: &str = env!("VERGEN_CARGO_TARGET_TRIPLE");
+const GIT_DESCRIBE: &str = env!("VERGEN_GIT_DESCRIBE");
+
+#[cfg(target_pointer_width = "64")]
+const PTR_SIZE: usize = 8;
+#[cfg(target_pointer_width = "32")]
+const PTR_SIZE: usize = 4;
+
+const MB: usize = 1024 * 1024;
 
 #[derive(supershorty::Args, Debug)]
 #[args(name = "nanoda", allow_no_args = true)]
@@ -46,12 +53,12 @@ fn main() -> ExitCode {
         Args::usage();
         return ExitCode::FAILURE;
     }
-    let memory_size_orig = args.memory_size.unwrap();
-    if memory_size_orig > usize::MAX as u64 / 8 / 1024 / 1024 {
+    let n_mb = args.memory_size.unwrap();
+    if n_mb > (usize::MAX / (PTR_SIZE * MB)) as u64 {
         eprintln!("`n` is too large");
         return ExitCode::from(EXIT_TOO_LARGE);
     }
-    let memory_size = (memory_size_orig as usize) * 1024 * 1024 / size_of::<usize>();
+    let memory_size = n_mb as usize * MB / PTR_SIZE;
     let iterations = args.iterations.unwrap();
     if iterations > usize::MAX as u64 {
         eprintln!("`i` is too large");
@@ -59,24 +66,26 @@ fn main() -> ExitCode {
     }
     let iterations = iterations as usize;
 
-    {
-        let version = if GIT_DESCRIBE == "VERGEN_IDEMPOTENT_OUTPUT" {
-            format!("v{} (cargo)", CARGO_PKG_VERSION)
+    println!(
+        "nanoda version:\tv{} (cargo), {} (git)\nbuild target:\t{}\nmemory size:\t{} MiB, test iterations {}",
+        CARGO_PKG_VERSION,
+        if GIT_DESCRIBE == "VERGEN_IDEMPOTENT_OUTPUT" {
+            "unknown"
         } else {
-            format!("{} (git)\tv{} (cargo)", GIT_DESCRIBE, CARGO_PKG_VERSION)
-        };
-        println!(
-            "nanoda version:\t{}\nbuild target:\t{}\nmemory size:\t{} MiB, test iterations {}",
-            version, TARGET_TRIPLE, memory_size_orig, iterations
-        );
-    }
+            GIT_DESCRIBE
+        },
+        TARGET_TRIPLE,
+        n_mb,
+        iterations
+    );
 
-    let mut data: Box<[usize]> = unsafe { Box::new_uninit_slice(memory_size).assume_init() };
-    let base = data.as_ptr();
+    let mut data = Box::new_uninit_slice(memory_size);
     for i in 0..memory_size {
-        data[i] = permute(i + 1, memory_size);
+        data[i].write(permute(i + 1, memory_size));
     }
-    let mut results = unsafe { Box::new_uninit_slice(iterations).assume_init() };
+    let data = unsafe { data.assume_init() };
+    let base = data.as_ptr();
+    let mut results = Box::new_uninit_slice(iterations);
     let clock = quanta::Clock::new();
     for r in 0..iterations {
         let mut p = r;
@@ -86,18 +95,19 @@ fn main() -> ExitCode {
             compiler_fence(Ordering::SeqCst);
         }
         let dt = t0.elapsed().as_secs_f64() * 1e9 / ITER as f64;
-        results[r] = dt;
+        results[r].write(dt);
         hint::black_box(p);
     }
 
+    let results = unsafe { results.assume_init() };
     let min = results.iter().copied().reduce(f64::min).unwrap();
     let max = results.iter().copied().reduce(f64::max).unwrap();
-    let mut format = format!("min = {min:.3} ns, max = {max:.3} ns");
+    print!("\nresults:\nmin = {min:.3} ns, max = {max:.3} ns");
     if iterations > 2 {
         let avg = results.iter().sum::<f64>() / results.len() as f64;
-        format.push_str(&format!(", avg = {avg:.3} ns"));
+        print!(", avg = {avg:.3} ns");
     }
-    println!("\nresults:\n{format}");
+    println!();
 
-    return ExitCode::SUCCESS;
+    ExitCode::SUCCESS
 }
